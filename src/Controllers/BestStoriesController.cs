bab89d6bcab908fd4e3bdb41d78d6fbbdeb9c102
agpcardoso <@Aceav223>
2026-02-26 23:41:49 -0300

    Add project files.

---
+++ src/Controllers/BestStoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SantanderTest.Services;
+
+namespace SantanderTest.Controllers
+{
+    [ApiController]
+    [Route("api/beststories")]
+    public class BestStoriesController : ControllerBase
+    {
+        private readonly IBestStoriesService _service;
+
+
+        public BestStoriesController(IBestStoriesService service)
+        {
+            _service = service;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetBestStories([FromQuery] int amount = 10)
+        {
+            if (amount <= 0) return BadRequest("amount must be greater than 0");
+            var result = await _service.GetBestStoriesAsync(amount);
+            return Ok(result);
+        }
+    }
+}
